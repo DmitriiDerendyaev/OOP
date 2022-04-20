50ec904affb1c365c832fd2fae734caca0428621
d.s.derendyaev <dd_stalker@mail.ru>
2022-04-20 17:02:59 +0300

    create a task_8 folder

---
+++ task_8/function_file.h
@@ -0,0 +1,59 @@
+void task1(void)
+{
+
+}
+
+void task2(void)
+{
+
+}
+
+void task3()
+{
+
+}
+
+void task4()
+{
+
+}
+
+void task5()
+{
+
+}
+
+void task6()
+{
+
+}
+
+void task7()
+{
+
+}
+
+void task8()
+{
+
+}
+
+void task9()
+{
+
+}
+
+void task10()
+{
+
+}
+
+void task11()
+{
+
+}
+
+void task12()
+{
+
+}
---
+++ task_8/task_8.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include "function_file.h"
+#include <cstring>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int task;
+	cout << "FAQ: Enter 0 for exit." << endl;
+	cout << "Enter the task number: ";
+	cin >> task;
+	while (task != 0)
+	{
+		switch (task)
+		{
+		case 1:
+		{
+			task1();
+			break;
+		}
+		case 2:
+		{
+			task2();
+			break;
+		}
+		case 3:
+		{
+			task3();
+			break;
+		}
+		case 4:
+		{
+			task4();
+			break;
+		}
+		case 5:
+		{
+			task5();
+			break;
+		}
+		case 6:
+		{
+			task6();
+			break;
+		}
+		case 7:
+		{
+			task7();
+			break;
+		}
+		case 8:
+		{
+			task8();
+			break;
+		}
+		case 9:
+		{
+			task9();
+			break;
+		}
+		case 10:
+		{
+			task10();
+			break;
+		}
+		case 11:
+		{
+			task11();
+			break;
+		}
+		case 12:
+		{
+			task12();
+			break;
+		}
+		}
+		cout << "Enter the task number: ";
+		cin >> task;
+	}
+	system("pause");
+}
