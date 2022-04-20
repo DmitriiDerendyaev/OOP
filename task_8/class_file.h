31fd96554d5556c27db7e06e285ff64fa126023a
d.s.derendyaev <dd_stalker@mail.ru>
2022-04-20 17:43:07 +0300

    stop working on this profect. move to task_8_divided

--- task_8/class_file.h
+++ task_8/class_file.h
@@ -1,5 +1,56 @@
-#pragma once
-class class_file
+#include <iostream>
+
+using namespace std;
+
+class publication // базовый класс
 {
+private:
+	string title;
+	float price;
+public:
+	void getdata()
+	{
+		cout << "\nВведите заголовок:"; cin >> title;
+		cout << "Введите цену:"; cin >> price;
+	}
+	void putdata()const
+	{
+		cout << "\nЗаголовок:" << title;
+		cout << "\nЦена:" << price;
+	}
+};
+
+class book :private publication // порожденный класс
+{
+private:
+	int pages;
+public:
+	void getdata()
+	{
+		publication::getdata();
+		cout << "Введите число страниц:"; cin >> pages;
+	}
+	void putdata()const
+	{
+		publication::putdata();
+		cout << "\nСтраниц:" << pages;
+	}
+};
+
+class tape :private publication // порожденный класс
+{
+private:
+	float time;
+public:
+	void getdata()
+	{
+		publication::getdata();
+		cout << "Введите время звучания:"; cin >> time;
+	}
+	void putdata()const
+	{
+		publication::putdata();
+		cout << "\nВремя звучания:" << time;
+	}
 };
 
--- task_8/function_file.h
+++ task_8/function_file.h
@@ -1,11 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+#include "class_file.h"
+#include "string_file.h"
+
+using namespace std;
+
 void task1(void)
 {
+	book book1; // определить публикации
+	tape tape1;
+	book1.getdata(); // получить данные о них
+	tape1.getdata();
+	book1.putdata(); // вывести данные о них
+	tape1.putdata();
+	cout << endl;
 
 }
 
 void task2(void)
 {
-
+	//Pstring s1("Это очень-очень длинная строка, которая, возможно, да, нет, точно, -- превысит размер, ограниченный SZ.");
+	//cout << "\ns1 ="; s1.display(); // вывести String
+	//Pstring s2 = "А это коротенькая строка.";// определить String
+	//cout << "\ns2 ="; s2.display(); // вывести String
+	//cout << endl;
 }
 
 void task3()
