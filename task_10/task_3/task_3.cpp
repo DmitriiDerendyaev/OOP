434959998273b73c815eccd2f767b112003eff31
d.s.derendyaev <dd_stalker@mail.ru>
2022-06-09 01:24:20 +0300

    strange solution, a minimum og changes from examle.....

--- task_10/task_3/task_3.cpp
+++ task_10/task_3/task_3.cpp
@@ -1,47 +1,43 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-////////////////////////////////////////////////////////////////
-class Distance //English Distance class
-{
+
+class Distance {
 private:
 	int feet;
 	float inches;
 public:
-	Distance() //constructor (no args)
-	{
-		feet = 0; inches = 0.0;
+	Distance() : feet(0), inches(0.0) {}//конструктор без аргумеентов
+	Distance(float fltfeet) {//конструктор перевода из float в distace
+		feet = static_cast<int>(fltfeet);
+		inches = 12 * (fltfeet - feet);
 	}
-	Distance(float fltfeet) //constructor (one arg)
-	{ //feet is integer part feet =
-		static_cast<int>(fltfeet); inches = 12 * (fltfeetfeet); //inches is what's left
-	} //constructor (two args)
-	Distance(int ft, float in) : feet(ft), inches(in)
-	{ }
-	void showdist() //display distance { cout <<
-		feet << "\'-" << inches << '\"';
-} friend Distance operator
-* (Distance, Distance); //friend
+	Distance(int ft, float in) : feet(ft), inches(in) {}//конструктор с двумя аргументами
+	void showdist() {
+		cout << feet << "\'-" << inches << '\"';
+	}
+	friend Distance operator * (Distance, Distance);//прототип дружественной перегрузки
 };
-//--------------------------------------------------------------
-//multiply d1 by d2
-Distance operator * (Distance d1, Distance d2)
-{
-	float fltfeet1 = d1.feet + d1.inches / 12; //convert to float
-	float fltfeet2 = d2.feet + d2.inches / 12; float multfeet =
-		fltfeet1 * fltfeet2; //find the product return
-	Distance(multfeet); //return temp Distance
+Distance operator * (Distance d1, Distance d2) {
+	float fltfeet1 = d1.feet + d1.inches / 12;
+	float fltfeet2 = d2.feet + d2.inches / 12;
+	float multfeet = fltfeet1 * fltfeet2;
+	return Distance(multfeet);
 }
-////////////////////////////////////////////////////////////////
-int main()
-{
-	Distance dist1(3, 6.0); //make some distances
+int main() {
+	Distance dist1(3, 6.0);
 	Distance dist2(2, 3.0);
 	Distance dist3;
-	dist3 = dist1 * dist2; //multiplication
-	dist3 = 10.0 * dist3; //mult and conversion
-	//display all distances
-	cout << "\ndist1 = "; dist1.showdist();
-	cout << "\ndist2 = "; dist2.showdist();
-	cout << "\ndist3 = "; dist3.showdist();
-	cout << endl; return 0;
+	Distance Wdist;
+	Wdist = 7.5 * dist2;
+	dist3 = dist1 * dist2;
+	dist3 = 10.0 * dist3;
+	cout << "dist1 = ";
+	dist1.showdist();
+	cout << "\ndist2 = ";
+	dist2.showdist();
+	cout << "\ndist3 = ";
+	dist3.showdist();
+	cout << "\nWdist = ";
+	Wdist.showdist();
+	return 0;
 }
